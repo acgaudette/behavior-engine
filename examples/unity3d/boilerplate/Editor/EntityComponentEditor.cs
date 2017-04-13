30a3c3df898a8284ed30f68efe85ee8b12ce5c91
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-04-13 05:00:22 -0400

    Added custom inspector for EntityComponent class
    Renders attributes nicely: uneditable, single-line with no dropdown

---
+++ examples/unity3d/boilerplate/Editor/EntityComponentEditor.cs
@@ -0,0 +1,44 @@
+// EntityComponentEditor.cs
+// Created by Aaron C Gaudette on 12.04.17
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EntityComponent))]
+public class EntityComponentEditor : Editor {
+
+  SerializedProperty debug, attributes;
+
+  void OnEnable() {
+    debug = serializedObject.FindProperty("debug");
+    attributes = serializedObject.FindProperty("attributes");
+  }
+
+  public override void OnInspectorGUI() {
+    serializedObject.Update();
+
+    EditorGUILayout.PropertyField(debug);
+
+    GUIStyle bold = new GUIStyle(EditorStyles.boldLabel);
+    EditorGUILayout.LabelField(
+      "Attributes (" + attributes.arraySize + ")", bold
+    );
+
+    EditorGUI.indentLevel++;
+    EditorGUIUtility.labelWidth = 64;
+
+    for (int i = 0; i < attributes.arraySize; ++i) {
+      SerializedProperty label
+        = attributes.GetArrayElementAtIndex(i).FindPropertyRelative("label");
+      SerializedProperty state
+        = attributes.GetArrayElementAtIndex(i).FindPropertyRelative("state");
+
+      EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(label.stringValue);
+        EditorGUILayout.Slider(state.floatValue, 0, 1);
+      EditorGUILayout.EndHorizontal();
+    }
+
+    serializedObject.ApplyModifiedProperties();
+  }
+}
