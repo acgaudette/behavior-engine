0b005280953e83ece5c2f867b5ee9c3169247f64
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-04-25 02:47:05 -0400

    Link PersonalityEffect with ICharacterAction
    Performs linked Action on Effect trigger

--- modules/personality/PersonalityEffect.cs
+++ modules/personality/PersonalityEffect.cs
@@ -8,23 +8,33 @@ namespace BehaviorEngine.Personality {
     HashSet<PersonalityProperty> strongPropertyInfluences;
     PersonalityProperty targetProperty;
 
+    ICharacterAction action;
+
     public PersonalityEffect(
       HashSet<PersonalityFactor> factors = null,
       HashSet<PersonalityProperty> properties = null,
-      PersonalityProperty target = null
+      PersonalityProperty target = null,
+      ICharacterAction action
     ) : base() {
       strongFactorInfluences = factors;
 
-      if(factors == null) {
+      if (factors == null) {
         strongFactorInfluences = new HashSet<PersonalityFactor>();
       }
       strongPropertyInfluences = properties;
 
-      if(properties == null) {
+      if (properties == null) {
         strongPropertyInfluences = new HashSet<PersonalityProperty>();
       }
 
       this.targetProperty = target;
+
+      this.action = action;
+    }
+
+    protected override void OnTrigger(Entity target, bool effective) {
+      if (action !=null)
+        action.Perform();
     }
   }
 }
