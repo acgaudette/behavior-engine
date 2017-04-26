199db96b29f4d1d5bc3eca53b4b3b57c0409f367
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-04-26 13:40:06 -0400

    Make EntityDebugger attachable
    Factor out lambda expressions and add Attach/Detach methods

--- src/debug/EntityDebugger.cs
+++ src/debug/EntityDebugger.cs
@@ -8,69 +8,95 @@ namespace BehaviorEngine.Debug {
 
   public static class EntityDebugger {
 
+#if BEHAVIORENGINE_DEBUG
+
+    static EntityEvents.OnReactEventHandler LogReaction = (
+      object sender,
+      Interaction interaction, IEntity host, IList<Effect> effects
+    ) => {
+      IEntity entity = sender as IEntity;
+
+      string debug = entity.GetLabel() + "\n";
+      debug += "REACTION\n";
+
+      debug += "Interaction = " + interaction.GetLabel() + "\n";
+      debug += "Host = " + host.GetLabel()
+        + (host == entity ? " (self)" : "") + "\n";
+
+      debug += Utility.CollectionToString(
+        "Effects", effects, e => e.GetVerboseLabel()
+      );
+
+      Debugger.Log(debug);
+    };
+
+    static EntityEvents.OnObserveEventHandler LogObservation = (
+      object sender,
+      Interaction interaction, IEntity host,
+      ICollection<IEntity> targets, IList<Effect> effects
+    ) => {
+      IEntity entity = sender as IEntity;
+
+      string debug = entity.GetLabel() + "\n";
+      debug += "OBSERVATION\n";
+
+      debug += "Interaction = " + interaction.GetLabel() + "\n";
+      debug += "Host = " + host.GetLabel() + "\n";
+
+      debug += Utility.CollectionToString(
+        "Targets", targets, t => t.GetLabel()
+      );
+      debug += Utility.CollectionToString(
+        "Effects", effects, e => e.GetVerboseLabel()
+      );
+
+      Debugger.Log(debug);
+    };
+
+    static EntityEvents.OnPollEventHandler LogPoll = (
+      object sender,
+      Interaction choice, ICollection<IEntity> targets, float highscore
+    ) => {
+      IEntity entity = sender as IEntity;
+
+      string debug = entity.GetLabel() + "\n";
+      debug += "POLL\n";
+
+      debug += "Interaction = "
+        + (choice == null ? "null" : choice.GetLabel()) + "\n";
+
+      debug += Utility.CollectionToString(
+        "Targets", targets, t => t.GetLabel()
+      );
+
+      debug += "Score = " + highscore + "\n";
+
+      Debugger.Log(debug);
+    };
+
+#endif
+
     [Conditional("BEHAVIORENGINE_DEBUG")]
     public static void Attach(IEntity entity) {
 
 #if BEHAVIORENGINE_DEBUG
 
-      entity.OnReact += (
-        object sender,
-        Interaction interaction, IEntity host, IList<Effect> effects
-      ) => {
-        string debug = entity.GetLabel() + "\n";
-        debug += "REACTION\n";
+      entity.OnReact += LogReaction;
+      entity.OnObserve += LogObservation;
+      entity.OnPoll += LogPoll;
 
-        debug += "Interaction = " + interaction.GetLabel() + "\n";
-        debug += "Host = " + host.GetLabel()
-          + (host == entity ? " (self)" : "") + "\n";
+#endif
 
-        debug += Utility.CollectionToString(
-          "Effects", effects, e => e.GetVerboseLabel()
-        );
+    }
 
-        Debugger.Log(debug);
-      };
+    [Conditional("BEHAVIORENGINE_DEBUG")]
+    public static void Detach(IEntity entity) {
 
-      entity.OnObserve += (
-        object sender,
-        Interaction interaction, IEntity host,
-        ICollection<IEntity> targets, IList<Effect> effects
-      ) => {
-        string debug = entity.GetLabel() + "\n";
-        debug += "OBSERVATION\n";
+#if BEHAVIORENGINE_DEBUG
 
-        debug += "Interaction = " + interaction.GetLabel() + "\n";
-        debug += "Host = " + host.GetLabel() + "\n";
-
-        debug += Utility.CollectionToString(
-          "Targets", targets, t => t.GetLabel()
-        );
-
-        debug += Utility.CollectionToString(
-          "Effects", effects, e => e.GetVerboseLabel()
-        );
-
-        Debugger.Log(debug);
-      };
-
-      entity.OnPoll += (
-        object sender,
-        Interaction choice, ICollection<IEntity> targets, float highscore
-      ) => {
-        string debug = entity.GetLabel() + "\n";
-        debug += "POLL\n";
-
-        debug += "Interaction = "
-          + (choice == null ? "null" : choice.GetLabel()) + "\n";
-
-        debug += Utility.CollectionToString(
-          "Targets", targets, t => t.GetLabel()
-        );
-
-        debug += "Score = " + highscore + "\n";
-
-        Debugger.Log(debug);
-      };
+      entity.OnReact -= LogReaction;
+      entity.OnObserve -= LogObservation;
+      entity.OnPoll -= LogPoll;
 
 #endif
 
