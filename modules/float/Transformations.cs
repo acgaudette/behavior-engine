0211e3b825b2bec98972c46bbae3b52805e450a8
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-04-27 06:08:28 -0400

    Add more transformations

--- modules/float/Transformations.cs
+++ modules/float/Transformations.cs
@@ -1,6 +1,8 @@
 // Transformations.cs
 // Created by Aaron C Gaudette on 26.05.17
 
+using System;
+
 using Transformation = BehaviorEngine.Float.NormalizedAttribute.Transform;
 
 namespace BehaviorEngine.Float {
@@ -10,6 +12,32 @@ namespace BehaviorEngine.Float {
 
     public static Transformation Linear() {
       return x => x;
+    }
+
+    public static Transformation Squared() {
+      return x => x * x;
+    }
+
+    public static Transformation InvertedSquared() {
+      return x => 1 - (1 - x) * (1 - x);
+    }
+
+    public static Transformation Power(float p) {
+      return x => (float)Math.Pow(x, p);
+    }
+
+    public static Transformation InvertedPower(float p) {
+      return x => (float)(1 - Math.Pow(1 - x, p));
+    }
+
+    public static Transformation EaseSquared() {
+      return x => x * x / (x * x + (1 - x) * (1 - x));
+    }
+
+    public static Transformation Ease(float p) {
+      return x => (float)(
+        Math.Pow(x, p) / (Math.Pow(x, p) + Math.Pow(1 - x, p))
+      );
     }
   }
 }
