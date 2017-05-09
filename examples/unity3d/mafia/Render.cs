e43351a4832fd64c7bd8590f842d1350a13a5416
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-05-09 01:38:30 -0400

    Add file logging
    Add log string list to Render
    Update log in Log() and Print() methods
    Clean messages of tags before updating log
    
    Add WriteToFile() method to Render
    - Writes log to file at input path

--- examples/unity3d/mafia/Render.cs
+++ examples/unity3d/mafia/Render.cs
@@ -1,22 +1,50 @@
 // Render.cs
 // Created by Aaron C Gaudette on 02.05.17
 
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Render {
 
   public const string ENDL = "\n";
 
+  static List<string> log = new List<string>();
+
   public static void Log(string observation, string analysis) {
     string timestamp = "[00:00:00]"; // stub
-    Debug.Log(
-      timestamp + ENDL
+
+    string entry = timestamp + ENDL
       + "observation: " + observation + ENDL
-      + "analysis: " + analysis + ENDL
-    );
+      + "analysis: " + analysis + ENDL;
+
+    Debug.Log(entry);
+    log.Add(Clean(entry));
   }
 
   public static void Print(string message) {
-    Debug.Log(message + ENDL);
+    string entry = message + ENDL;
+
+    Debug.Log(entry);
+    log.Add(Clean(entry));
+  }
+
+  public static bool WriteToFile(string path) {
+    try {
+      File.WriteAllLines(@path, log.ToArray());
+    } catch (Exception e) {
+      Debug.LogError("Render: " + e);
+      return false;
+    }
+
+    Debug.Log("Render: Wrote " + log.Count + " lines to file");
+
+    return true;
+  }
+
+  static string Clean(string entry) {
+    return Regex.Replace(entry, "<.*?>", "");
   }
 }
