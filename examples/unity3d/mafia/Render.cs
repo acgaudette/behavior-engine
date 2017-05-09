05cfef9fc02164fbb27eb2161ad3984e599557fc
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-05-09 02:38:55 -0400

    Render proper timestamp
    Add Timestamp struct to Render
    - Includes cycle, tick, and offset ints
    - Includes a value padding method and overrides ToString()
    
    Update static timestamp in Render (preliminary implementation)
    - Randomly offset seconds, renders as cycle:minutes:seconds

--- examples/unity3d/mafia/Render.cs
+++ examples/unity3d/mafia/Render.cs
@@ -1,7 +1,6 @@
 // Render.cs
 // Created by Aaron C Gaudette on 02.05.17
 
-using System;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Collections.Generic;
@@ -11,12 +10,33 @@ public class Render {
 
   public const string ENDL = "\n";
 
+  public struct Timestamp {
+
+    public int cycle, tick, offset;
+
+    public Timestamp(int cycle, int tick, int offset) {
+      this.cycle = cycle;
+      this.tick = tick;
+      this.offset = offset;
+    }
+
+    public override string ToString() {
+      return "[" + Pad(cycle) + ":" + Pad(tick) + ":" + Pad(offset) + "]";
+    }
+
+    string Pad(int i) {
+      return i < 10 ? "0" + i : i.ToString();
+    }
+  }
+
   static List<string> log = new List<string>();
 
-  public static void Log(string observation, string analysis) {
-    string timestamp = "[00:00:00]"; // stub
+  static int cycle = 0, tick = 0, offset = 0;
 
-    string entry = timestamp + ENDL
+  public static void Log(
+    string observation, string analysis
+  ) {
+    string entry = GetTimestamp() + ENDL
       + "observation: " + observation + ENDL
       + "analysis: " + analysis + ENDL;
 
@@ -34,7 +54,7 @@ public class Render {
   public static bool WriteToFile(string path) {
     try {
       File.WriteAllLines(@path, log.ToArray());
-    } catch (Exception e) {
+    } catch (System.Exception e) {
       Debug.LogError("Render: " + e);
       return false;
     }
@@ -42,6 +62,24 @@ public class Render {
     Debug.Log("Render: Wrote " + log.Count + " lines to file");
 
     return true;
+  }
+
+  public static void IncrementCycle() {
+    cycle++;
+    tick = 0;
+    offset = 0;
+  }
+
+  static Timestamp GetTimestamp() {
+    int seconds = Random.Range(3, 15);
+
+    if (offset + seconds > 60)
+      tick++;
+
+    offset += seconds;
+    offset %= 60;
+
+    return new Timestamp(cycle, tick, offset);
   }
 
   static string Clean(string entry) {
